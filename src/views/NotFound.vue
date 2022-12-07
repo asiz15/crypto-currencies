6216303249bcee9c948fcd2e2fd9a3500b57af03
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:55:09 -0300

    Create NotFound view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <div class="not-found-page d-flex flex-column justify-content-center align-items-center">
+        <h1 class="fs-1">404</h1>
+        <h2 class="fs-3">Not found</h2>
+        <RouterLink to="/" class="btn btn-primary mt-3">Back to home</RouterLink>
+    </div>
+</template>
+<style lang="scss" scoped>
+.not-found-page{ 
+    height: 90vh;
+}
+</style>
