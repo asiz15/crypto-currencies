e7e5cf1b976e10085a0f03f01ababd2d03b90205
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:58:33 -0300

    Create CoinItemSkeleton component

---
+++ src/components/skeletons/CoinItemSkeleton.vue
@@ -0,0 +1,29 @@
+<template>
+    <article class="coin-item-skeleton row px-2 py-3 m-0 border-bottom d-flex align-items-center placeholder-glow">
+        <div class="col col-2 col-md-1 d-none d-md-flex text-center justify-content-center p-0">
+            <span class="placeholder w-25 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-5 col-md-3 text-start d-flex align-items-center">
+            <span class="placeholder avatar rounded-circle me-3"></span>
+            <span class="placeholder w-75 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-5 col-md-2 text-center fs-7">
+            <span class="placeholder w-50 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-3 col-md-2 d-none d-md-block text-center fs-7">
+            <span class="placeholder w-50 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-2 col-md-1 d-none d-md-block text-center p-0 fs-7">
+            <span class="placeholder w-25 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-1 col-md-1 d-none d-md-block text-center p-0 fs-7">
+            <span class="placeholder w-25 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-1 col-md-1 d-none d-md-block text-center p-0 fs-7">
+            <span class="placeholder w-25 text-skeleton rounded"></span>
+        </div>
+        <div class="col col-2 col-md-1 text-center p-0 fs-7">
+            <span class="placeholder w-25 text-skeleton rounded"></span>
+        </div>
+    </article>
+</template>
