5f08dcd8227066afea92802996c5879fd513be1a
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:59:44 -0300

    Create Breadcrumbs and Breadcrumbs skeleton component

---
+++ src/components/navigation/Breadcrumbs.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+    currentCoin: {
+        type: String,
+        required: true
+    }
+})
+</script>
+<template>
+    <nav class="px-3 mt-3 mb-5">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item fs-7 text-black-50">
+                <RouterLink to="/">Crypto currencies </RouterLink>
+            </li>
+            <li class="breadcrumb-item fs-7 text-black">{{ props.currentCoin }}</li>
+        </ol>
+    </nav>
+</template>
---
+++ src/components/skeletons/BreadcrumbsSkeleton.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav  class="px-3 mt-3 mb-5">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item fs-7 text-black-50">
+                    <span class="placeholder rounded" style="width:100px"></span>
+                </li>
+                <li class="breadcrumb-item text-black-50">
+                    <span class="placeholder rounded" style="width:50px"></span>
+                </li>
+            </ol>
+        </nav>
+</template>
