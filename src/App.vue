6920e3c22fe63c506ba3e34576a6c4f34cf1b908
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:55:44 -0300

    Set app general layout

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import NavBar from './components/navigation/NavBar.vue'
+import AppFooter from './components/navigation/Footer.vue'
+</script>
+
+<template>
+  <main class="min-vh-100">
+    <NavBar />
+    <RouterView />
+    <AppFooter />
+  </main>
+</template>
+
