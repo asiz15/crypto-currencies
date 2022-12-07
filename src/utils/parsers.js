16aa17ee5d728833df86010655db1a931a6d36a6
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:59:13 -0300

    Create parser

---
+++ src/utils/parsers.js
@@ -0,0 +1,37 @@
+
+// Based on most accepted answer in stackoverflow 
+// https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
+export const priceParser = (price) => {
+    if(!price) return '0'
+    const roundedPrice = parseFloat(price).toFixed(2)
+    const parsePrice = (p => {
+        return  p.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    })
+    return roundedPrice > 0 ? parsePrice(roundedPrice) : parsePrice(parseFloat(price).toFixed(10))
+}
+
+export const percentageParser = (percentage) => {
+    
+    return !percentage ? 0 : parseFloat(percentage).toFixed(2)
+}
+
+export const largeCurrencyParser = (ammount, abbreviated = true) => {
+
+    if(!ammount) return '0 USD'
+
+    // Nine Zeroes for Billions
+    return Math.abs(+ammount) >= 1.0e+9
+
+    ? (Math.abs(+ammount) / 1.0e+9).toFixed(2) + (abbreviated ? ' B' : ' Billions')
+    // Six Zeroes for Millions 
+    : Math.abs(+ammount) >= 1.0e+6
+
+    ? (Math.abs(+ammount) / 1.0e+6).toFixed(2) + (abbreviated ? ' M' : ' Millions')
+    // Three Zeroes for Thousands
+    : Math.abs(+ammount) >= 1.0e+3
+
+    ? (Math.abs(+ammount) / 1.0e+3).toFixed(2) + (abbreviated ? ' K' : ' Thousands')
+
+    : Math.abs(+ammount).toFixed(8) + ' USD'
+
+}
