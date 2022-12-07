084da7fd61413859af0727e796912b8992ecf520
asiz15 <a.silvazapata@gmail.com>
2022-12-07 18:56:50 -0300

    Create NavBar and Footer components

---
+++ src/components/navigation/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer fixed bottom app-footer bg-dark p-2 py-3 text-center text-white">
+        <span class="fs-7">Made with <font-awesome-icon icon="heart" class="mx-1 text-danger" /> by <a href="https://github.com/asiz15" target="_blank">asiz15</a></span>
+    </footer>
+</template>
+<style lang="scss">
+.app-footer{
+    position: relative;
+    bottom: 0;
+}
+</style>
---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const handleRouteChange = () => {
+    router.push({path: '/', query: { page: 1}})
+}
+</script>
+<template>
+    <header class="w-100">
+        <nav class="navbar navbar-dark bg-dark mt-0 ms-0 w-100">
+            <div class="container">
+                <div class="d-flex mx-auto" @click="handleRouteChange">
+                    <img src="@/assets/logo.png" width="50" class="me-2" />
+                    <span class="navbar-brand mb-0 fs-6 fw-bold" role="button" >Crypto Currencies</span>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
